a020744d15cc03390abba2e11182e825a46eba3d
neyema <neyema@outlook.com>
2019-12-07 15:48:17 +0200

    MIT-define, checked that the sexpr created is fine :)

--- tag-parser.ml
+++ tag-parser.ml
@@ -65,6 +65,8 @@ let rec tag_parse sexpr =
 (* | Pair (Symbol "or", lst) -> Or ( tag_parse ) *)
 | Pair (Symbol "if", Pair (test, Pair (dit, Nil))) -> If (tag_parse test, tag_parse dit, Const(Void))
 | Pair (Symbol "if", Pair (test, Pair (dit, Pair (dif, Nil)))) -> If (tag_parse test, tag_parse dit, tag_parse dif)
+| Pair (Symbol "define", Pair( Pair(Symbol name, args), body)) ->
+              tag_parse (Pair( Symbol "define", Pair( Symbol name, Pair( Symbol "lambda", Pair (args, body)))))
 | Pair (Symbol "define", Pair (Symbol name, Pair (sexpr, Nil))) -> Def (tag_parse (Symbol name), tag_parse sexpr)
 | Pair (Symbol "quote", Pair (x, Nil)) -> Const (Sexpr x)
 | Pair (exp1, rest) -> Applic ((tag_parse exp1), List.fold_right (fun x acc -> List.cons x acc) (pairstoList rest) [])
