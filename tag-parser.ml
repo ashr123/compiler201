10d2c12f7e862fc6d5548b3795cd1863bd8f0de7
ashr123 <roy.ash456@gmail.com>
2019-12-09 01:59:42 +0200

    did tag_parse_expressions

--- tag-parser.ml
+++ tag-parser.ml
@@ -19,26 +19,26 @@ type expr =
 let rec expr_eq e1 e2 =
   match e1, e2 with
   | Const Void, Const Void -> true
-  | Const(Sexpr s1), Const(Sexpr s2) -> sexpr_eq s1 s2
-  | Var(v1), Var(v2) -> String.equal v1 v2
-  | If(t1, th1, el1), If(t2, th2, el2) -> (expr_eq t1 t2) &&
-                                          (expr_eq th1 th2) &&
-                                          (expr_eq el1 el2)
-  | (Seq(l1), Seq(l2)
-    | Or(l1), Or(l2)) -> List.for_all2 expr_eq l1 l2
-  | (Set(var1, val1), Set(var2, val2)
-    | Def(var1, val1), Def(var2, val2)) -> (expr_eq var1 var2) &&
-                                           (expr_eq val1 val2)
-  | LambdaSimple(vars1, body1), LambdaSimple(vars2, body2) ->
-    (List.for_all2 String.equal vars1 vars2) &&
-    (expr_eq body1 body2)
-  | LambdaOpt(vars1, var1, body1), LambdaOpt(vars2, var2, body2) ->
-    (String.equal var1 var2) &&
-    (List.for_all2 String.equal vars1 vars2) &&
-    (expr_eq body1 body2)
-  | Applic(e1, args1), Applic(e2, args2) ->
-    (expr_eq e1 e2) &&
-    (List.for_all2 expr_eq args1 args2)
+  | Const (Sexpr s1), Const (Sexpr s2) -> sexpr_eq s1 s2
+  | Var v1, Var v2 -> String.equal v1 v2
+  | If (t1, th1, el1), If (t2, th2, el2) -> expr_eq t1 t2 &&
+                                          expr_eq th1 th2 &&
+                                          expr_eq el1 el2
+  | Seq l1, Seq l2
+    | Or l1, Or l2 -> List.for_all2 expr_eq l1 l2
+  | Set (var1, val1), Set (var2, val2)
+    | Def (var1, val1), Def (var2, val2) -> expr_eq var1 var2 &&
+                                           expr_eq val1 val2
+  | LambdaSimple (vars1, body1), LambdaSimple (vars2, body2) ->
+    List.for_all2 String.equal vars1 vars2 &&
+    expr_eq body1 body2
+  | LambdaOpt (vars1, var1, body1), LambdaOpt (vars2, var2, body2) ->
+    String.equal var1 var2 &&
+    List.for_all2 String.equal vars1 vars2 &&
+    expr_eq body1 body2
+  | Applic (e1, args1), Applic (e2, args2) ->
+    expr_eq e1 e2 &&
+    List.for_all2 expr_eq args1 args2
   | _ -> false;;
 
 
@@ -49,7 +49,7 @@ module type TAG_PARSER =  sig
   val tag_parse_expressions : sexpr list -> expr list
 end;; (* signature TAG_PARSER *)
 
-module Tag_Parser (*: TAG_PARSER*) = struct
+module Tag_Parser : TAG_PARSER = struct
 
   let reserved_word_list =
     ["and"; "begin"; "cond"; "define"; "else";
@@ -250,7 +250,7 @@ module Tag_Parser (*: TAG_PARSER*) = struct
 
   and tag_parse_expression sexpr = tag_parse sexpr
 
-  and tag_parse_expressions sexpr = raise X_not_yet_implemented;;
+  and tag_parse_expressions sexprs = List.map tag_parse_expression sexprs;;
 
 end;; (* struct Tag_Parser *)
 
