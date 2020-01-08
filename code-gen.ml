6b508217cecdcb0b0435f72e6e94a0f661080aec
neyema <neyema@post.bgu.ac.il>
2020-01-08 17:45:56 +0200

    lambda-simple and applic are done :) great work by me

--- code-gen.ml
+++ code-gen.ml
@@ -167,21 +167,26 @@ module Code_Gen : CODE_GEN = struct
       label ^ string_of_int !count (* this is outside the if expression *)
   ;;
 
-  let label_Lelse_counter = counterGenerator "Lelse"
-  and label_Lexit_counter = counterGenerator "Lexit"
-  and label_Lcode_counter = counterGenerator "Lcode"
-  and label_Lcont_counter = counterGenerator "Lcont"
+  let label_Lelse_counter = counterGenerator "else"
+  and label_Lexit_counter = counterGenerator "exit"
+  and label_Lcode_counter = counterGenerator "code"
+  and label_Lcont_counter = counterGenerator "cont"
+  and label_CopyEnvLoop_counter = counterGenerator "copy_env_loop"
+  and label_CopyParams_counter = counterGenerator "copy_params_loop"
+  and label_AfterEnvCopy_counter = counterGenerator "after_env_copy"
+  and label_MakeClosure_counter = counterGenerator "make_closure"
+  and label_ApplicProcIsColusre = counterGenerator "applic_proc_is_colsure"
   ;;
 
   let get_offset_fvar table var =
     List.fold_left (fun index (s,off) -> if (index>(-1)) then index else (if (s=var) then off else index)) (-1) table
   ;;
 
-  let rec generateRec consts fvars e =
+  let rec generateRec consts fvars e envSize =
     match e with
     | Const' constant -> "mov rax, const_tbl + " ^ string_of_int (get_offset_of_const consts constant) ^ "\n"
     | Var' (VarFree s) -> "mov rax, [fvar_tbl+" ^ string_of_int (get_offset_fvar fvars s) ^ "]\n"
-    | Seq' exprlist -> List.fold_left (fun acc expr' -> acc ^ generateRec consts fvars expr') "" exprlist
+    | Seq' exprlist -> List.fold_left (fun acc expr' -> acc ^ generateRec consts fvars expr' envSize) "" exprlist
     (* very very important !!!!!
        the labels generator will evaluate in undefined order, so make sure by hand that all calls to counter are in the right order *)
     | If' (test, dit, dif) ->
@@ -191,61 +196,117 @@ module Code_Gen : CODE_GEN = struct
       let elseLabel = label_Lelse_counter false
       and exitLabel = label_Lexit_counter false
       in
-      generateRec consts fvars test ^
+      generateRec consts fvars test envSize ^
       "cmp rax, SOB_FALSE_ADDRESS\n" ^
       "je " ^ elseLabelWithInc ^ "\n" ^
-      generateRec consts fvars dit ^
+      generateRec consts fvars dit envSize ^
       "jmp " ^ exitLabelWithInc ^ "\n" ^
       elseLabel ^ ":\n" ^
-      generateRec consts fvars dif ^
+      generateRec consts fvars dif envSize ^
       exitLabel ^ ":\n"
     | Or' exprlist ->
       let exitLabelWithInc = label_Lexit_counter true
       and exitLabel = (label_Lexit_counter false)
-      in let first = ((generateRec consts fvars (List.hd exprlist))^"cmp rax, SOB_FALSE_ADDRESS\njne "^exitLabelWithInc^"\n")
+      in let first = ((generateRec consts fvars (List.hd exprlist) envSize)^"cmp rax, SOB_FALSE_ADDRESS\njne "^exitLabelWithInc^"\n")
       in let (acc,_) =
         List.fold_left
         (fun (acc,index) curr ->
-          let newacc = (generateRec consts fvars curr) ^
+          let newacc = (generateRec consts fvars curr envSize) ^
           (if (index > List.length exprlist) then (exitLabel^":\n") else ("cmp rax, SOB_FALSE_ADDRESS\njne "^exitLabel^"\n"))
           in (acc^newacc, index+1))
         (first ,2) exprlist
       in acc
     | Def' (Var' (VarFree s), expr) ->
-      (generateRec consts fvars expr)
+      (generateRec consts fvars expr envSize)
       ^ "mov qword [fvar_tbl+" ^ string_of_int (get_offset_fvar fvars s) ^ "], rax\n"
       ^ "mov rax, SOB_VOID_ADDRESS\n"
     | Set' (Var' (VarFree s), expr) ->
-      (generateRec consts fvars expr)
+      (generateRec consts fvars expr envSize)
       ^ "mov qword [fvar_tbl+" ^ string_of_int (get_offset_fvar fvars s) ^ "], rax\n"
       ^ "mov rax, SOB_VOID_ADDRESS\n"
     | LambdaSimple' (params, body) ->
-      let start = ""
-      (*
-      Create ExtEnv
-      Allocate closure object
-      Closure → Env ≔ ExtEnv
-      Closure → Code ≔ Lcode
-      *)
-      in
-      let codeLabelWithInc = label_Lcode_counter true
+      let copyEnvLoopWithInc = label_CopyEnvLoop_counter true
+      and copyEnvLoopLabel = label_CopyEnvLoop_counter false
+      and copyParamsLoopWithInc = label_CopyParams_counter true
+      and copyParamsLoopLabel = label_CopyParams_counter false
+      and codeLabelWithInc = label_Lcode_counter true
+      and codeLabel = label_Lcode_counter false
       and contLabelWithInc = label_Lcont_counter true
       and contLabel = label_Lcont_counter false
+      and afterEnvCopyWithInc = label_AfterEnvCopy_counter true
+      and afterEnvCopyLabel = label_AfterEnvCopy_counter false
+      and makeClosureWithInc = label_MakeClosure_counter true
+      and makeClosureLabel = label_MakeClosure_counter false
       in
-      start ^
+      let code =
+        (* allocate new env, so rax <- the address to the extended env*)
+        "MALLOC rax, WORD_SIZE * " ^ string_of_int (envSize+1) ^ "\n" ^ (*rax <- address to ExtEnv*)
+        (*copy env*)
+        "mov rbx, [rbp + 2 * WORD_SIZE]\n" ^ (*now rbx holds the pointer to the previous env*)
+        "mov rcx, " ^ string_of_int envSize ^"\n" ^
+        "cmp rcx, 0\n" ^
+        "jle " ^ afterEnvCopyWithInc ^ "\n" ^
+        copyEnvLoopWithInc ^ ":\n" ^ (*rcx will go from n...1*)
+        "\tmov rdx, [rbx + WORD_SIZE * rcx - WORD_SIZE]\n" ^
+        "\tmov [rax + WORD_SIZE * rcx], rdx\n" ^
+        "\tloop " ^ copyEnvLoopLabel ^ "\n" ^
+        (* now we'll peform ExtEnv[0] -> vector with params *)
+        afterEnvCopyLabel ^ ":\n" ^
+        "mov rbx, rax\n" ^ (* rbx <- ExtEnv*)
+        "mov rax, WORD_SIZE\n" ^
+        "mov rcx, [rbp + 3 * WORD_SIZE]\n" ^  (* rcx<-n from the stack*)
+        "mul rcx\n" ^ (* rax <- n*WORD_SIZE*)
+        "MALLOC rdx, rax\n" ^ (* rdx <- address to new vector*)
+        "mov [rbx], rdx\n" ^ (* ExtEnv[0] -> new vector *)
+        "mov rcx, [rbp + 3 * WORD_SIZE]\n" ^  (* rcx<-n from the stack*)
+        "cmp rcx, 0\n" ^
+        "jle " ^ makeClosureWithInc ^ "\n" ^
+        copyParamsLoopWithInc ^ ":\n" ^  (*rcx will go from n...1*)
+        "\tmov rax, [rbp + 4 * WORD_SIZE + WORD_SIZE * rcx - WORD_SIZE]\n" ^ (* rax <- param(rcx-1) *)
+        "\tmov [rdx + WORD_SIZE * rcx - WORD_SIZE], rdx\n" ^ (* new vector[rcx-1] <- param(rcx-1) *)
+        "\tloop " ^ copyParamsLoopLabel ^ "\n" ^
+      (* Allocate closure object *)
+      (* Closure → Env ≔ ExtEnv *)
+      (* Closure → Code ≔ Lcode *)
+        makeClosureLabel ^ ": MAKE_CLOSURE(rax, rbx, " ^ codeLabelWithInc ^ ")\n"
+      in
+      code ^
       "jmp " ^ contLabelWithInc ^ "\n" ^
-      codeLabelWithInc ^ ":\n" ^
+      (*Lcode label, this is a piece of code that is not executed now, just written for the closure*)
+      codeLabel ^ ":\n" ^
       "\tpush rbp\n" ^
       "\tmov rbp, rsp\n" ^
+      (generateRec consts fvars body (envSize+1)) ^
       "\tleave\n" ^
-      "\ret\n" ^
+      "\tret\n" ^
       contLabel ^ ":\n"
+    | Applic' (proc, args) ->
+      let applicProcIsClosureWithInc = label_ApplicProcIsColusre true
+      and applicProcIsClosure = label_ApplicProcIsColusre false
+      in 
+      List.fold_left (fun acc arg -> acc ^ (generateRec consts fvars arg envSize) ^ "push rax\n") "" args ^
+      "push " ^ string_of_int(List.length args) ^ "\n" ^
+      (generateRec consts fvars proc envSize) ^
+      "cmp byte [rax], T_CLOSURE\n
+      je " ^ applicProcIsClosureWithInc ^ "\n" ^
+      (* what to do when proc is not a clousre *)
+      "mov rax, 0\nadd rsp, 4*8\npop rbp\nret\n" ^
+      (* what to do when proc is a closure*)
+      applicProcIsClosure ^ ":
+      \tCLOSURE_ENV rbx, rax
+      \tpush rbx
+      \tCLOSURE_CODE rbx, rax
+      \tcall rbx
+      \tadd rsp, 8*1 ;pop env
+      \tpop rbx ;pop arg count
+      \tshl rbx, 3 ;rbx = rbx * 8
+      \tadd rsp, rbx ;pop args\n"
     | _ -> raise X_not_yet_implemented
     ;;
 
   (* creates assembly code for single expr', these strings will concat
      the prolog will contains the section .data init of const_tbl and freevar_tbl *)
-  let generate consts fvars e = generateRec consts fvars e ;;
+  let generate consts fvars e = generateRec consts fvars e 0;;
 end;;
 
 (*tests*)
@@ -268,17 +329,7 @@ Seq'
      BoxSet' (VarBound ("d", 0, 3), Const' (Sexpr (Number (Int 1)))))]]));;
 Code_Gen.make_fvars_tbl [expr'];;
 *)
-(* Code_Gen.label_Lelse_counter true;;
-   Code_Gen.label_Lelse_counter true;;
-   Code_Gen.label_Lelse_counter true;;
-   Code_Gen.label_Lelse_counter false;;
-   Code_Gen.label_Lexit_counter true;;
-   Code_Gen.label_Lexit_counter true;;
-   Code_Gen.label_Lexit_counter true;; *)
 
-(*let expr' = Const' (Sexpr (String "abc"));;
-Code_Gen.generate (Code_Gen.make_consts_tbl [expr']) (Code_Gen.make_fvars_tbl [expr']) expr';;*)
-  
 (*let expr' = Def' (Var' (VarFree "a"), Const' (Sexpr (Number (Int 3))));;
 Code_Gen.make_fvars_tbl [expr'];;
 *)
