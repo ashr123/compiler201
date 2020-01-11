3d6b624220e68bfb8eab098b8d494be2c4515631
Roy Ash <roy.ash456@gmail.com>
2020-01-11 03:14:11 +0200

    3 functions

--- stdlib.scm
+++ stdlib.scm
@@ -1,18 +1,20 @@
 (define map
   (let ((null? null?)
-	(car car) (cdr cdr)
-	(cons cons) (apply apply))
+        (car car)
+        (cdr cdr)
+        (cons cons)
+        (apply apply))
     (letrec ((map-loop (lambda (f l . ls)
-		     (if (null? l)
-			 '() ; simplifying assumption: if l is empty, then ls is also empty
-			 (if (null? ls)
-			     (cons (f (car l)) (map-loop f (cdr l)))
-			     (cons (apply f (car l) (map-loop car ls))
-				   (apply map f (cdr l) (map-loop cdr ls))))))))
+                         (if (null? l)
+                             '() ; simplifying assumption: if l is empty, then ls is also empty
+                             (if (null? ls)
+                                 (cons (f (car l)) (map-loop f (cdr l)))
+                                 (cons (apply f (car l) (map-loop car ls))
+                                       (apply map f (cdr l) (map-loop cdr ls))))))))
       map-loop)))
 
 (define fold-left
-   (let ((car car)
+  (let ((car car)
         (cdr cdr)
 		(null? null?))
     (lambda (f z xs)
@@ -21,24 +23,25 @@
           (fold-left f (f z (car xs)) (cdr xs))))))
 
 (define fold-right
-   (let ((car car)
+  (let ((car car)
         (cdr cdr)
 		(null? null?))
     (lambda (f z xs)
       (if (null? xs)
           z
           (f (car xs) (fold-right f z (cdr xs)))))))
-          
+
 (define append
   (let ((null? null?)
-	(fold-right fold-right)
-	(cons cons))
+        (fold-right fold-right)
+        (cons cons))
     (lambda args
       (fold-right (lambda (e a)
-		    (if (null? a)
-			e
-			(fold-right cons a e)))
-		  '() args))))
+                    (if (null? a)
+                        e
+                        (fold-right cons a e)))
+                  '()
+				  args))))
 
 (define last-element
   (let ((null? null?)
@@ -58,7 +61,7 @@
       (if (null? (cdr l))
           '()
           (cons (car l) (init (cdr l)))))))
-          
+
 (define cons*
   (let ((null? null?)
         (append append))
@@ -67,131 +70,168 @@
           '()
           (append (init lst) (last-element lst))))))
 
-(define list (lambda x x))
+(define list
+  (lambda x
+    x))
 
 (define list?
   (let ((null? null?)
-	(pair? pair?)
-	(cdr cdr))
+        (pair? pair?)
+        (cdr cdr))
     (letrec ((list?-loop (lambda (x)
-			   (or (null? x)
-			       (and (pair? x)
-				    (list? (cdr x)))))))
+                           (or (null? x)
+                               (and (pair? x)
+                                    (list? (cdr x)))))))
       list?-loop)))
 
 (define length
   (let ((fold-left fold-left)
-	(+ +))
+        (+ +))
     (lambda (l)
-      (fold-left (lambda (acc e) (+ acc 1)) 0 l))))
+      (fold-left (lambda (acc e)
+                   (+ acc 1))
+                 0
+                 l))))
 
 (define make-string
-  (let ((null? null?) (car car)
-	(make-string make-string))
+  (let ((null? null?)
+        (car car)
+        (make-string make-string))
     (lambda (x . y)
       (if (null? y)
-	  (make-string x #\nul)
-	  (make-string x (car y))))))
+          (make-string x #\nul)
+          (make-string x (car y))))))
 
 (define not
-  (lambda (x) (if x #f #t)))
+  (lambda (x)
+    (if x
+        #f
+        #t)))
 
 (define number?
   (let ((float? float?)
-	(integer? integer?))
+        (integer? integer?))
     (lambda (x)
       (or (float? x) (integer? x)))))
 
 (define +
   (let ((fold-left fold-left)
-	(+ +))
-    (lambda x (fold-left + 0 x))))
+        (+ +))
+    (lambda x
+      (fold-left + 0 x))))
 
 (define *
   (let ((fold-left fold-left)
-	(* *))
-    (lambda x (fold-left * 1 x))))
+        (* *))
+    (lambda x
+      (fold-left * 1 x))))
 
 (define -
   (let ((apply apply)
-	(- -) (+ +)
-	(null? null?))
+        (- -)
+        (+ +)
+        (null? null?))
     (lambda (x . y)
       (if (null? y)
-	  (- 0 x)
-	  (- x (apply + y))))))
+          (- 0 x)
+          (- x (apply + y))))))
 
 (define /
   (let ((apply apply)
-	(/ /) (* *)
-	(null? null?))
+        (/ /)
+        (* *)
+        (null? null?))
     (lambda (x . y)
       (if (null? y)
-	  (/ 0 x)
-	  (/ x (apply * y))))))
+          (/ 0 x)
+          (/ x (apply * y))))))
 
 (define =
-  (let ((= =) (null? null?)
-	(car car) (cdr cdr)
-	(apply apply))
+  (let ((= =)
+        (null? null?)
+		(car car)
+        (cdr cdr)
+		(apply apply))
     (letrec ((=-loop (lambda (x . y)
-		       (if (null? y)
-			   #t ; simplifying assumption: x is a number
-			   (and (= x (car y)) (apply =-loop x (cdr y)))))))
+                       (if (null? y)
+                           #t ; simplifying assumption: x is a number
+                           (and (= x (car y)) (apply =-loop x (cdr y)))))))
       =-loop)))
 
 (define <
-  (let ((null? null?) (< <)
-	(car car) (cdr cdr))
+  (let ((null? null?)
+        (< <)
+        (car car)
+        (cdr cdr))
     (letrec ((<-loop (lambda (element lst)
-		     (if (null? lst)
-			 #t
-			 (and (< element (car lst))
-			     (<-loop (car lst) (cdr lst)))))))
+                       (if (null? lst)
+                           #t
+                           (and (< element (car lst))
+                                (<-loop (car lst) (cdr lst)))))))
       (lambda (x . y)
-	(<-loop x y)))))
+        (<-loop x y)))))
 
 (define >
-  (let ((null? null?) (< <) (= =)
-	(not not) (car car) (cdr cdr))
+  (let ((null? null?)
+        (< <)
+        (= =)
+        (not not)
+        (car car)
+        (cdr cdr))
     (letrec ((>-loop (lambda (element lst)
-		     (if (null? lst)
-			 #t
-			 (and (not (or
-				    (< element (car lst))
-				    (= element (car lst))))
-			      (>-loop (car lst) (cdr lst)))))))
+                       (if (null? lst)
+                           #t
+                           (and (not (or (< element (car lst))
+                                         (= element (car lst))))
+                                (>-loop (car lst) (cdr lst)))))))
       (lambda (x . y)
-	(>-loop x y)))))
+        (>-loop x y)))))
 
 (define zero?
   (let ((= =))
-    (lambda (x) (= x 0))))
+    (lambda (x)
+      (= x 0))))
 
 (define string->list
   (let ((string-ref string-ref)
-	(string-length string-length)
-	(< <) (- -))
+        (string-length string-length)
+        (< <)
+        (- -))
     (lambda (s)
       (letrec ((s->l-loop (lambda (n a)
-			    (if (< n 0)
-				a
-				(s->l-loop (- n 1) (cons (string-ref s n) a))))))
-	(s->l-loop (- (string-length s) 1) '())))))
+                            (if (< n 0)
+                                a
+                                (s->l-loop (- n 1) (cons (string-ref s n) a))))))
+        (s->l-loop (- (string-length s) 1) '())))))
 
 (define equal?
-  (let ((= =) (string->list string->list)
-	(integer? integer?) (float? float?)
-	(pair? pair?) (char? char?)
-	(string? string?) (eq? eq?)
-	(car car) (cdr cdr)
-	(char->integer char->integer))
+  (let ((= =)
+        (string->list string->list)
+		(integer? integer?)
+        (float? float?)
+        (pair? pair?)
+        (char? char?)
+        (string? string?)
+        (eq? eq?)
+        (car car)
+        (cdr cdr)
+        (char->integer char->integer))
     (letrec ((equal?-loop (lambda (x y)
-			    (or
-			     (and (integer? x) (integer? y) (= x y))
-			     (and (float? x) (float? y) (= x y))
-			     (and (pair? x) (pair? y) (equal?-loop (car x) (car y)) (equal?-loop (cdr x) (cdr y)))
-			     (and (char? x) (char? y) (= (char->integer x) (char->integer y)))
-			     (and (string? x) (string? y) (equal?-loop (string->list x) (string->list y)))
-			     (eq? x y)))))
-    equal?-loop)))
+                            (or (and (integer? x)
+                                     (integer? y)
+                                     (= x y))
+                                (and (float? x)
+                                     (float? y)
+                                     (= x y))
+                                (and (pair? x)
+                                     (pair? y)
+                                     (equal?-loop (car x) (car y))
+                                     (equal?-loop (cdr x) (cdr y)))
+                                (and (char? x)
+                                     (char? y)
+                                     (= (char->integer x) (char->integer y)))
+                                (and (string? x)
+                                     (string? y)
+                                     (equal?-loop (string->list x) (string->list y)))
+                                (eq? x y)))))
+      equal?-loop)))
